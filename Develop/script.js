42553e3f86561f715aaa6f782849f5ec863692f1
Jeremy Huynh <jeahuynh18@gmail.com>
2021-10-07 20:08:08 -0700

    added scripting for questions 1-5

--- Develop/script.js
+++ Develop/script.js
@@ -1,34 +1,36 @@
 // Assignment code here
-//var pwLowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"]
-//var pwLowercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-//var pwNumbers = [0,1,2,3,4,5,6,7,8,9]
-//var pwSpecials = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","]","{","}","|",";",""
+var generateBtn = document.querySelector("#generate");
 
-var pwCharacters = window.prompt("How many characters long would you like your password to be? (Min: 8, Max 128");
-  if (8 <= i && i <= 128) {
-    var pwLength = i
+var pwLowercase = "abcdefghijklmnopqrstuvwxyz"
+var pwUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var pwNumbers = "0123456789"
+var pwSpecials = "`~!@#$%^&*()-_=+[]{}|;"
+var genPassword = ""
+
+pwLength = window.prompt("How many characters long would you like your password to be? (Min: 8, Max 128");
+  while (8 > pwLength || pwLength > 128) {
+     window.alert("Please choose a number between 8 and 128.")
   }
 
-
-var pwLowerCase = window.prompt("Would you like to include lower-case letters in your password? (Y/N)");
-  if (pwLowercase === "Y" || pwLowerCase === "YES") {
-    ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"]
+userInput = window.prompt("Would you like to include lower-case letters in your password? (1 for Yes, 0 for No)");
+  if (userInput == 1){
+    genPassword += pwLowercase
   }
-  else (pwLowercase === "N" || pwLowercase === "NO") {
-    ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"]
 
+userInput = window.prompt("Would you like to include UPPER-CASE letters in your password? (1 for Yes, 0 for No)");
+  if (userInput == 1) {
+    genPassword += pwUppercase
   }
-var pwUpperCase = window.prompt("Would you like to include UPPER-CASE letters in your password? (Y/N)");
-  if (pwUpperCase === "Y" || pwUpperCase === "YES") {
-    ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+userInput = window.prompt("Would you like to include numbers in your password? (1 for Yes, 0 for No)");
+  if (userInput == 1) {
+    genPassword += pwNumbers
   }
-  else (pwUpperCase === "N" || pwUpperCase === "NO")
-    ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-var pwNumbers = window.prompt("Would you like to include numbers in your password? (Y/N)");
-
-var pwSpecials = window.prompt("Would you like to include special characters (!,@,#,$,%,etc.) in your password? (Y/N)");
-
+userInput = window.prompt("Would you like to include special characters (!,@,#,$,%,etc.) in your password? (1 for Yes, = for No)");
+  if (userInput == 1) {
+    genPassword += pwSpecials
+  }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
