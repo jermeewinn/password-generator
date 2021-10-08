91f24e21ea3696f2b26d3f64b1018c8c3d6ad429
Jeremy Huynh <jeahuynh18@gmail.com>
2021-10-07 21:56:13 -0700

    added section notes

--- Develop/script.js
+++ Develop/script.js
@@ -5,28 +5,29 @@ var pwUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var pwNumbers = "0123456789"
 var pwSpecials = "`~!@#$%^&*()-_=+[]{}|;"
 var genPassword = ""
-
+//Click generate password to start the prompt. This section asks for length of the password. If invalid, it will ask you to re-enter a valid input. If invalid inputs are repeated, it will contiue to ask until a valid input is entered.
 var generatePassword = function() {
   var pwLength = window.prompt("How many characters long would you like your password to be? (Min: 8, Max 128");
     while (8 > pwLength || pwLength > 128) {
      pwLength = window.prompt("Please choose a number between 8 and 128.")
   }
-
+  //Asks for lower-case letters. 1 denotes yes. Anything else (including words, strings, etc.) denotes no.
   userInput = window.prompt("Would you like to include lower-case letters in your password? (1 for Yes, 0 for No)");
   if (userInput == 1){
     genPassword += pwLowercase
   }
+  //Asks for UPPER-CASE letters. 1 denotes yes. Anything else (including words, strings, etc.) denotes no.
 
   userInput = window.prompt("Would you like to include UPPER-CASE letters in your password? (1 for Yes, 0 for No)");
   if (userInput == 1) {
     genPassword += pwUppercase
   }
-
+  //Asks for numbers letters. 1 denotes yes. Anything else (including words, strings, etc.) denotes no.
   userInput = window.prompt("Would you like to include numbers in your password? (1 for Yes, 0 for No)");
   if (userInput == 1) {
     genPassword += pwNumbers
   }
-
+  //Asks for special-characters. 1 denotes yes. Anything else (including words, strings, etc.) denotes no.
   userInput = window.prompt("Would you like to include special characters (!,@,#,$,%,etc.) in your password? (1 for Yes, = for No)");
   if (userInput == 1) {
     genPassword += pwSpecials
