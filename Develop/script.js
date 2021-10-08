6da8e7ca26ab497639a778920f4eac917dc1efa6
Jeremy Huynh <jeahuynh18@gmail.com>
2021-10-07 21:12:33 -0700

    added for loop referencing all the other parameters

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
 var pwLowercase = "abcdefghijklmnopqrstuvwxyz"
 var pwUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -7,8 +7,9 @@ var pwNumbers = "0123456789"
 var pwSpecials = "`~!@#$%^&*()-_=+[]{}|;"
 var genPassword = ""
 
-pwLength = window.prompt("How many characters long would you like your password to be? (Min: 8, Max 128");
-  while (8 > pwLength || pwLength > 128) {
+var generatePassword = function() {
+  var pwLength = window.prompt("How many characters long would you like your password to be? (Min: 8, Max 128");
+    while (8 > pwLength || pwLength > 128) {
      window.alert("Please choose a number between 8 and 128.")
   }
 
@@ -30,17 +31,21 @@ userInput = window.prompt("Would you like to include numbers in your password? (
 userInput = window.prompt("Would you like to include special characters (!,@,#,$,%,etc.) in your password? (1 for Yes, = for No)");
   if (userInput == 1) {
     genPassword += pwSpecials
-  }
+  } return pwLength;
+}
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate")
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword(pwLength) {
+  var password = generatePassword(); 
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  var temporaryString = "";
+  for (var i = 0; i < password; i++){
+    temporaryString += genPassword[Math.floor(Math.random()*genPassword.length)]
+  }
+  passwordText.value = temporaryString;
+  
 }
 
 // Add event listener to generate button
